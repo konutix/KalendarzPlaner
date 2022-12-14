c96f2c4b832cc2bdb0acadfd01f18e15d54f1b7d
patrykludwisiak <pkl9pkl@gmail.com>
2022-12-14 18:56:49 +0100

    Graphical fixes

--- Assets/Scripts/FilterPanel.cs
+++ Assets/Scripts/FilterPanel.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class FilterPanel : MonoBehaviour
 {
+
+    RectTransform rect;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rect = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameObject.activeSelf)
+        {
+            if (Input.mousePosition.x < rect.position.x + rect.rect.xMin ||
+                Input.mousePosition.x > rect.position.x + rect.rect.xMax ||
+                Input.mousePosition.y < rect.position.y + rect.rect.yMin ||
+                Input.mousePosition.y > rect.position.y + rect.rect.yMax)
+            {
+                if(Input.GetMouseButton(0))
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
     }
 
     public void ToggleHide()
