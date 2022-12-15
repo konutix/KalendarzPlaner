3b3ce0abd876cf821001a7a43b10582fe7078f7c
konutix <konutix@gmail.com>
2022-12-15 14:07:16 +0100

    coloring current day

--- Assets/mainView/MainViewDays.cs
+++ Assets/mainView/MainViewDays.cs
@@ -22,6 +22,8 @@ public class MainViewDays : MonoBehaviour
 
     public void SetupMonth()
     {
+        System.DateTime dateNow = System.DateTime.UtcNow.ToLocalTime();
+
         System.DateTime dateFirst = new System.DateTime(dateCurrent.Year, dateCurrent.Month, 1, 1, 1, 1, 1, dateCurrent.Kind);
 
         int firstDay = 0;
@@ -54,6 +56,11 @@ public class MainViewDays : MonoBehaviour
                     day.GetComponentInChildren<Text>().text = dayIterator.ToString();
                     day.gameObject.GetComponent<RectTransform>().anchoredPosition =
                     new Vector2(xBegin + d * daySize + d * xPadding, -yBegin - w * daySize - w * yPadding);
+
+                    if(dateCurrent.Year == dateNow.Year && dateCurrent.Month == dateNow.Month && dateNow.Day == dayIterator)
+                    {
+                        day.GetComponent<Image>().color = new Color32(120, 142, 225, 255);
+                    }
                 }
             }
         }
